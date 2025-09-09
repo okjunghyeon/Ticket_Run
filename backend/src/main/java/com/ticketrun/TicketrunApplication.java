579c2f3123ecef76e360c4b99a0a22891c0486c4
jeonghyeon-ohk <ok6737@naver.com>
2025-09-09 20:16:14 +0900

    init

---
+++ backend/src/main/java/com/ticketrun/TicketrunApplication.java
@@ -0,0 +1,13 @@
+package com.ticketrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketrunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketrunApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ticketrun/TicketrunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketrunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
